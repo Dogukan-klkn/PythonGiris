9486ebde88bf5f90ea5e10dbbd5fcbb7656d1325
Doğukan Kalkan <dogukankalkanfrt@gmail.com>
2024-09-27 20:43:36 +0300

    Ptyhon-Giris
    Python dilinde oluştrulmuş başlangıç seviye algoritmik sorular

---
+++ Odev1/odev1-1.py
@@ -0,0 +1,18 @@
+#Ödev1-1: Değişkenlere atanmış değerlerin veri tipleri arasında dönüşüm yapılır.
+int_var = 10        # Integer
+float_var = 20.5    # Float
+str_var = "30"      # String
+
+float_to_int = int(float_var)    
+int_to_str = str(int_var)      
+str_to_float = float(str_var)    
+
+
+print("Float to Int:", float_to_int)
+print("Int to Str:", int_to_str)
+print("Str to Float:",str_to_float)
+
+
+
+
+
